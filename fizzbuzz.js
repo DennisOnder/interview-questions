b35a34101ddc9538e6e405e773d0a3a53a6f91f1
Dennis Onder <webdev.dennisonder@gmail.com>
2019-05-29 10:17:26 +0200

    Fizzbuzz fixed

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,9 +1,9 @@
 for (let i = 1; i <= 100; i++) {
-  i % 3 == 0
-    ? console.log("Fizz")
+  i % 15 == 0
+    ? console.log("FizzBuzz")
     : i % 5 == 0
     ? console.log("Buzz")
-    : i % 15 == 0
-    ? console.log("Fizzbuzz")
+    : i % 3 == 0
+    ? console.log("Fizz")
     : console.log(i);
 }
